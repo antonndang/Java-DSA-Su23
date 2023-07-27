520eb5d905927b7e0b0851ebe8592fb1ae769e9e
antonndang <adang99@uw.edu>
2023-07-26 17:06:23 -0700

    done

--- heap/src/priorityqueues/ArrayHeapMinPQ.java
+++ heap/src/priorityqueues/ArrayHeapMinPQ.java
@@ -1,7 +1,15 @@
 package priorityqueues;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.ST;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Set;
 
 /**
  * @see ExtrinsicMinPQ
@@ -9,13 +17,16 @@ import java.util.List;
 public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
     // IMPORTANT: Do not rename these fields or change their visibility.
     // We access these during grading to test your code.
-    static final int START_INDEX = 1;
+    static final int START_INDEX = 0;
     List<PriorityNode<T>> items;
-    // TODO: add fields as necessary
+    private Map<T, Integer> itemMap; // maps the item to the index in the heap
 
     public ArrayHeapMinPQ() {
         items = new ArrayList<>();
-        // TODO: add code as necessary
+        for (int i = 0; i < START_INDEX; i++) {
+            items.add(null);
+        }
+        itemMap = new HashMap<>();
     }
 
     // Here's a method stub that may be useful. Feel free to change or remove it, if you wish.
@@ -23,44 +34,146 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
     /**
      * A helper method for swapping the items at two indices of the array heap.
      */
+
+    private void checkHeapIndex(int a) {
+        if (a < START_INDEX || a >= START_INDEX + size()) {
+            throw new NoSuchElementException();
+        }
+    }
     private void swap(int a, int b) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        checkHeapIndex(a);
+        checkHeapIndex(b);
+        // swap in the heap and in the items map
+        PriorityNode<T> temp = items.get(a);
+        items.set(a, items.get(b));
+        items.set(b, temp);
+        itemMap.put(items.get(a).getItem(), a);
+        itemMap.put(items.get(b).getItem(), b);
+    }
+
+    private int getParentIndex(int a) {
+        switch (START_INDEX) {
+            case 0: return (a - 1) / 2;
+            case 1: return a / 2;
+            default: throw new IllegalStateException("Only 0/1 start index supported");
+        }
+    }
+
+    private int getLeftChildIndex(int a) {
+        switch (START_INDEX) {
+            case 0: return a * 2 + 1;
+            case 1: return a * 2;
+            default: throw new IllegalStateException("Only 0/1 start index supported");
+        }
+    }
+
+    private int getRightChildIndex(int a) {
+        switch (START_INDEX) {
+            case 0: return a * 2 + 2;
+            case 1: return a * 2 + 1;
+            default: throw new IllegalStateException("Only 0/1 start index supported");
+        }
+    }
+
+    private int getFirstLeafIndex() {
+        switch (START_INDEX) {
+            case 0: return size() / 2;
+            case 1: return size() / 2 + 1;
+            default: throw new IllegalStateException("Only 0/1 start index supported");
+        }
+    }
+
+    private void siftUp(int a) {
+        while (a > START_INDEX) {
+            int p = getParentIndex(a);
+            if (items.get(a).getPriority() < items.get(p).getPriority()) {
+                swap(a, p);
+                a = p;
+            } else {
+                break; // no need to swap anymore invariant restored
+            }
+        }
+    }
+
+    private void siftDown(int a) {
+        int firstLeaf = getFirstLeafIndex();
+        while (a < firstLeaf) {
+            // presume left child has the lowest priority
+            int minChildIndex = getLeftChildIndex(a);
+            // check the right child too
+            int rightChildIndex = getRightChildIndex(a);
+            if (rightChildIndex < START_INDEX + size() &&
+                items.get(rightChildIndex).getPriority() < items.get(minChildIndex).getPriority()) {
+                minChildIndex = rightChildIndex;
+            }
+            // check if we need to swap with "a" (parent)
+            if (items.get(minChildIndex).getPriority() < items.get(a).getPriority()) {
+                swap(a, minChildIndex);
+                a = minChildIndex;
+            } else {
+                break; // no more swaps break
+            }
+        }
     }
 
     @Override
     public void add(T item, double priority) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (contains(item)) {
+            throw new IllegalArgumentException();
+        }
+        if (size() == START_INDEX + items.size()) {
+            // need additional storage
+            items.add(new PriorityNode<>(item, priority));
+        } else {
+            // set the last item
+            items.set(START_INDEX + size(), new PriorityNode<>(item, priority));
+        }
+        // update the item map
+        itemMap.put(item, START_INDEX + size());
+        // restore the heap invariant for the last index
+        siftUp(START_INDEX + size() - 1);
     }
 
     @Override
     public boolean contains(T item) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return itemMap.containsKey(item);
     }
 
     @Override
     public T peekMin() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (size() == 0) {
+            throw new NoSuchElementException();
+        }
+        return items.get(START_INDEX).getItem();
     }
 
     @Override
     public T removeMin() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        T result = peekMin();
+        itemMap.remove(result); // reduced the size by 1
+        items.set(START_INDEX, items.get(START_INDEX + itemMap.size())); // move the last item to START_INDEX
+        siftDown(START_INDEX); // Restore the invariant
+        return result;
     }
 
     @Override
     public void changePriority(T item, double priority) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (!contains(item)) {
+            throw new NoSuchElementException();
+        }
+        int itemIndex = itemMap.get(item);
+        PriorityNode<T> node = items.get(itemIndex);
+        double oldPriority = node.getPriority();
+        node.setPriority(priority);
+        if (priority > oldPriority) {
+            siftDown(itemIndex);
+        } else if (priority < oldPriority) {
+            siftUp(itemIndex);
+        }
     }
 
     @Override
     public int size() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return itemMap.size();
     }
 }
