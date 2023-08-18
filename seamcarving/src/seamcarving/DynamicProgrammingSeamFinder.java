4b5b713ddbea063384b419570402d6fb557adf2d
antonndang <adang99@uw.edu>
2023-08-17 22:04:50 -0700

    done

--- seamcarving/src/seamcarving/DynamicProgrammingSeamFinder.java
+++ seamcarving/src/seamcarving/DynamicProgrammingSeamFinder.java
@@ -3,7 +3,6 @@ package seamcarving;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Dynamic programming implementation of the {@link SeamFinder} interface.
