a7a2d63277c53442f90839bfa735d81c0f411981
antonndang <adang99@uw.edu>
2023-08-17 21:12:49 -0700

    done

--- seamcarving/src/seamcarving/DijkstraSeamFinder.java
+++ seamcarving/src/seamcarving/DijkstraSeamFinder.java
@@ -17,6 +17,9 @@ public class DijkstraSeamFinder implements SeamFinder {
     // x = index % nCols
     class ImageGraph implements Graph<Integer, Edge<Integer>> {
         // Negative values for the virtual verices
+        public static final int LEFT_TO_RIGHT_GRAPH = 0;
+        public static final int TOP_TO_BOTTOM_GRAPH = 1;
+        private int graphType;
         public static final int IMAGE_TOP = -1;
         public static final int IMAGE_BOTTOM = -2;
         public static final int IMAGE_LEFT = -3;
@@ -38,6 +41,16 @@ public class DijkstraSeamFinder implements SeamFinder {
         public Collection<Edge<Integer>> outgoingEdgesFrom(Integer vertex) {
             // based ont the image return the collection of edges
             // first check the special vertices
+            switch (graphType) {
+                case LEFT_TO_RIGHT_GRAPH:
+                    return leftToRightOutgoing(vertex);
+                case TOP_TO_BOTTOM_GRAPH:
+                    return topToBottomOutgoing(vertex);
+                default: throw new IllegalArgumentException();
+            }
+        }
+
+        private Collection<Edge<Integer>> topToBottomOutgoing(Integer vertex) {
             ArrayList<Edge<Integer>> result = new ArrayList<>();
             switch (vertex) {
                 case IMAGE_TOP:
@@ -46,25 +59,66 @@ public class DijkstraSeamFinder implements SeamFinder {
                         result.add(new Edge<>(IMAGE_TOP, getIndexFromXY(x, 0), data[x][0]));
                     }
                     break;
-                case IMAGE_BOTTOM:
-                    for (int x = 0; x < nCols; x++) {
-                        result.add(new Edge<>(IMAGE_BOTTOM, getIndexFromXY(x, nRows - 1), data[x][nRows - 1]));
-                    }
-                    break;
+                default:
+                    updateImagePixelNeighborsTopBottom(vertex, result);
+            }
+            return result;
+
+        }
+
+        private void updateImagePixelNeighborsTopBottom(Integer vertex, ArrayList<Edge<Integer>> result) {
+            int curX = getXFromIndex(vertex);
+            int curY = getYFromIndex(vertex);
+
+            if (curY == nRows - 1) {
+                // connect to the top side
+                result.add(new Edge<>(vertex, IMAGE_BOTTOM, 0));
+            } else {
+                result.add(new Edge<>(vertex, getIndexFromXY(curX, curY + 1), data[curX][curY + 1]));
+            }
+
+            if (curX > 0 && curY < nRows - 1) {
+                result.add(new Edge<>(vertex, getIndexFromXY(curX - 1, curY + 1), data[curX - 1][curY + 1]));
+            }
+            if (curX < nCols - 1 && curY < nRows - 1) {
+                result.add(new Edge<>(vertex, getIndexFromXY(curX + 1, curY + 1), data[curX + 1][curY + 1]));
+            }
+
+        }
+
+        private Collection<Edge<Integer>> leftToRightOutgoing(Integer vertex) {
+            ArrayList<Edge<Integer>> result = new ArrayList<>();
+            switch (vertex) {
                 case IMAGE_LEFT:
                     for (int y = 0; y < nRows; y++) {
                         result.add(new Edge<>(IMAGE_LEFT, getIndexFromXY(0, y), data[0][y]));
                     }
                     break;
-                case IMAGE_RIGHT:
-                    for (int y = 0; y < nRows; y++) {
-                        result.add(new Edge<>(IMAGE_RIGHT, getIndexFromXY(nCols - 1, y), data[nCols - 1][y]));
-                    }
-                    break;
                 default:
-                    updateImagePixelNeighbors(vertex, result);
+                    updateImagePixelNeighborsLeftRight(vertex, result);
             }
             return result;
+
+        }
+
+        private void updateImagePixelNeighborsLeftRight(Integer vertex, ArrayList<Edge<Integer>> result) {
+            int curX = getXFromIndex(vertex);
+            int curY = getYFromIndex(vertex);
+
+
+            if (curX == nCols - 1) {
+                // connect to the right side
+                result.add(new Edge<>(vertex, IMAGE_RIGHT, 0));
+            } else {
+                result.add(new Edge<>(vertex, getIndexFromXY(curX + 1, curY), data[curX + 1][curY]));
+            }
+            if (curX < nCols - 1 && curY > 0) {
+                result.add(new Edge<>(vertex, getIndexFromXY(curX + 1, curY - 1), data[curX + 1][curY - 1]));
+            }
+            if (curX < nCols - 1 && curY < nRows - 1) {
+                result.add(new Edge<>(vertex, getIndexFromXY(curX + 1, curY + 1), data[curX + 1][curY + 1]));
+            }
+
         }
 
         private void updateImagePixelNeighbors(Integer vertex, ArrayList<Edge<Integer>> result) {
@@ -114,7 +168,7 @@ public class DijkstraSeamFinder implements SeamFinder {
             }
         }
 
-        public ImageGraph(double[][] image) {
+        public ImageGraph(double[][] image, int graphType) {
              nRows = image[0].length;
              nCols = image.length;
              data = new double[nCols][nRows];
@@ -122,6 +176,15 @@ public class DijkstraSeamFinder implements SeamFinder {
                  for (int col = 0; col < nCols; col++) {
                      data[col][row] = image[col][row];
                  }
+             }
+             switch (graphType) {
+                case LEFT_TO_RIGHT_GRAPH:
+                    this.graphType = LEFT_TO_RIGHT_GRAPH;
+                    break;
+                case TOP_TO_BOTTOM_GRAPH:
+                    this.graphType = TOP_TO_BOTTOM_GRAPH;
+                    break;
+                 default: throw new IllegalArgumentException();
              }
         }
     }
@@ -141,7 +204,7 @@ public class DijkstraSeamFinder implements SeamFinder {
 
     @Override
     public List<Integer> findHorizontalSeam(double[][] energies) {
-        ImageGraph graph = new ImageGraph(energies);
+        ImageGraph graph = new ImageGraph(energies, ImageGraph.LEFT_TO_RIGHT_GRAPH);
         ShortestPath<Integer, Edge<Integer>> sp = pathFinder.findShortestPath(graph,
             ImageGraph.IMAGE_LEFT, ImageGraph.IMAGE_RIGHT);
         ArrayList<Integer> result = new ArrayList<>();
@@ -155,7 +218,7 @@ public class DijkstraSeamFinder implements SeamFinder {
 
     @Override
     public List<Integer> findVerticalSeam(double[][] energies) {
-        ImageGraph graph = new ImageGraph(energies);
+        ImageGraph graph = new ImageGraph(energies, ImageGraph.TOP_TO_BOTTOM_GRAPH);
         ShortestPath<Integer, Edge<Integer>> sp = pathFinder.findShortestPath(graph,
             ImageGraph.IMAGE_TOP, ImageGraph.IMAGE_BOTTOM);
         ArrayList<Integer> result = new ArrayList<>();
